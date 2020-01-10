8ad697dd5237044143f11728b1d0a5fe620be97a
Amal Azazi <36468144+AmalAzazi@users.noreply.github.com>
2020-01-10 12:34:08 +0800

    Add files via upload

---
+++ PizzeOrderingSystem/src/CheesePizza.java
@@ -0,0 +1,10 @@
+
+public class CheesePizza extends Pizza {
+public CheesePizza() {
+	name= "CheesePizza";
+	dough= "Regular Crust";
+	sauce= "Marinara Sauce";
+	toppings.add("Fresh Mozzarella");
+	toppings.add("Parmesan");
+}
+}
